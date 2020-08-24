22efff13ea7647c57af9f36906356c1dd49ae9ae
Joshua Wilensky <joshwilensky@gmail.com>
2020-08-24 18:22:08 -0400

    changes

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,29 @@
-const FILES_TO_CACHE = ["/", "/index.html", "index.js", "manifest.webmanifest"];
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/manifest.webmanifest',
+    '/styles.css',
+    '/index.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    // 'models/transaction.js',
+    // 'routes/api.js'
+];
+
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
-
-// INSTALL
-self.addEventListener("install", function (evt) {
+// to install and register your service worker
+self.addEventListener('install', function (evt) {
     evt.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
             console.log("Your files were pre-cached successfully!");
             return cache.addAll(FILES_TO_CACHE);
         })
     );
+
     self.skipWaiting();
 });
-
-// ACTIVATE
+// remove old data from the cache
 self.addEventListener("activate", function (evt) {
     evt.waitUntil(
         caches.keys().then(keyList => {
@@ -31,31 +41,32 @@ self.addEventListener("activate", function (evt) {
     self.clients.claim();
 });
 
-// FETCH
-self.addEventListener("fetch", function (evt) {
-    if (evt.request.url.includes("/api/")) {
+// Enable the service worker to intercept network requests
+self.addEventListener('fetch', function (evt) {
+    if (evt.request.url.includes('/api/')) {
+        console.log('[Service Worker] Fetch (data)', evt.request.url);
         evt.respondWith(
             caches.open(DATA_CACHE_NAME).then(cache => {
                 return fetch(evt.request)
                     .then(response => {
-                        // IF THE RESPONSE WAS GOOD, CLONE IT AND STORE IT IN THE CACHE
                         if (response.status === 200) {
                             cache.put(evt.request.url, response.clone());
                         }
-                        return response;
+                        return response
                     })
                     .catch(err => {
-                        // NETWORK REQUEST FAILED, TRY TO GET IT FROM THE CACHE
-                        return cache.match(evt.request);
-                    });
-            }).catch(err => console.log(err))
-        );
-        return;
+                        return cache.match(evt.request)
+                    })
+            })
+        )
+        return
     }
-
+    //Serve static files from the cache. Proceed with a network request when the resource is not in the cache. This code allows the page to be accessible offline. (This code should be placed in the function handling the `fetch` event.)
     evt.respondWith(
-        caches.match(evt.request).then(function (response) {
-            return response || fetch(evt.request);
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(evt.request).then(response => {
+                return response || fetch(evt.request);
+            });
         })
     );
 });
